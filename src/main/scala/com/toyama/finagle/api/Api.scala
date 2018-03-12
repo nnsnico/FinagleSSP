6f9d3323eb4fbf01a6970e24a84e52cbc465b393
Takayuki Sakai <t_sakai@fancs.com>
2018-03-12 16:01:19 +0900

    Awaitダメ！絶対！

--- src/main/scala/com/toyama/finagle/api/Api.scala
+++ src/main/scala/com/toyama/finagle/api/Api.scala
@@ -18,7 +18,7 @@ import io.circe.parser._
 import scala.language.postfixOps
 
 object Api extends App {
-  def requestToDsp(host: String, port: String, json: String): Future[Response] = {
+  def requestToDsp(host: String, port: String, json: String): Future[Either[Throwable, Response]] = {
     val client: Service[Request, Response] = Http.client
       .withRequestTimeout(100 milliseconds)
       .newService(s"""$host:$port""")
@@ -27,49 +27,34 @@ object Api extends App {
     request.setContentString(json)
     request.setContentTypeJson()
 
-    val response: Future[Response] = client(request)
-    response
+    client(request)
+      .map(Right(_))
+      .handle { case t => Left(t) } // Requestが失敗しても、FutureはFailureにならないようにする
   }
 
   val response: Endpoint[SspAdResBody] = post("v1" :: "ad" :: jsonBody[SspAdReqBody]) {
     (request: SspAdReqBody) =>
       val listOfFutures = List(requestToDsp("ss.matome-plus.info", "80", request.toString))
-      //      val futureOfList: Future[Seq[Response]] = Future.collect(listOfFutures)
-//      val a = for {
-//        responseNel <- listOfFutures.par.flatMap(awaitEachFuture).toList.toNel
-//      } yield {
-//        val dspAdResBodyList = for {
-//          response <- responseNel.toList
-//          dspAdResBody <- decode[DspAdResBody](response.contentString).toOption
-//        } yield dspAdResBody
-//
-//        dspAdResBodyList.toNel.map { dspAdResBodyNel =>
-//          Ok(
-//            SspAdResBody(dspAdResBodyNel.toList.maxBy(body => body.price).url)
-//          )
-//        }.getOrElse(NoContent)
-//      }
-//      a.getOrElse(NoContent)
+      val futureOfList = Future.collect(listOfFutures)
 
-      println(awaitEachFuture(listOfFutures.head))
-//      println(listOfFutures.par.flatMap(awaitEachFuture).toList.toNel)
-      val dspAdResList: List[DspAdResBody] = (for {
-        responseNel <- OptionT.fromOption[List](listOfFutures.par.flatMap(awaitEachFuture).toList.toNel)
-        response <- OptionT.liftF(responseNel.toList)
-        dspAdResBody <- OptionT.fromOption(decode[DspAdResBody](response.contentString).toOption)
-      } yield dspAdResBody).value.flatten
+      futureOfList.map { listOfEither =>
+        val responsesReceivedInTime = listOfEither.flatMap(_.toOption)
 
-      dspAdResList.toNel.map { dspAdResNel =>
-        Ok(
-          SspAdResBody(dspAdResNel.toList.maxBy(body => body.price).url)
-        )
-      }.getOrElse(NoContent)
+        val dspAdResList: List[DspAdResBody] = (for {
+          responseNel <- OptionT.fromOption[List](responsesReceivedInTime.toList.toNel)
+          response <- OptionT.liftF(responseNel.toList)
+          dspAdResBody <- OptionT.fromOption(decode[DspAdResBody](response.contentString).toOption)
+        } yield dspAdResBody).value.flatten
+
+        dspAdResList.toNel.map { dspAdResNel =>
+          Ok(
+            SspAdResBody(dspAdResNel.toList.maxBy(body => body.price).url)
+          )
+        }.getOrElse(NoContent)
+      }
   }
 
   val server: ListeningServer = Http.server.serve(":9000", response.toServiceAs[Application.Json])
 
   Await.ready(server)
-
-  private def awaitEachFuture(f: Future[Response]): Option[Response] =
-    Try(Await.result(f, 1000 millisecond)).toOption
 }
